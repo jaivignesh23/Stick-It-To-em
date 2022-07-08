c221e4080da3c7c39fe13f6bdbec7e64f0a0a480
ayushdj <ayushdj13@gmail.com>
2022-07-08 18:29:18 -0400

    rendering stickers from the database

--- app/src/main/java/edu/neu/madcourse/stick_it_to_em/ChatActivityViewHolder.java
+++ app/src/main/java/edu/neu/madcourse/stick_it_to_em/ChatActivityViewHolder.java
@@ -17,47 +17,49 @@ public class ChatActivityViewHolder extends RecyclerView.ViewHolder {
     public TextView userMessage;
     public RelativeLayout msgSentLayoutId;
     public RelativeLayout msgRcdLayoutId;
+    public ImageView stickerImg;
 
     //public ImageView stickerImg;
 
     public ChatActivityViewHolder(View ChatActivityView) {
         super(ChatActivityView);
-        this.userDate = ChatActivityView.findViewById(R.id.messageDate);
-        this.userMessage =  ChatActivityView.findViewById(R.id.messageContent);
+//        this.userDate = ChatActivityView.findViewById(R.id.messageDate);
+//        this.userMessage =  ChatActivityView.findViewById(R.id.messageContent);
         this.msgSentLayoutId = ChatActivityView.findViewById(R.id.msgSentLayoutId);
         this.msgRcdLayoutId = ChatActivityView.findViewById(R.id.msgRcdLayoutId);
+        this.stickerImg = ChatActivityView.findViewById(R.id.STICKER_IMAGE);
     }
 
     public void bindListData(ChatActivityData chatItem, String username) {
 
         // TODO : If the username is same as the sender name
+        RelativeLayout.LayoutParams paramsMsg = (RelativeLayout.LayoutParams)stickerImg.getLayoutParams();
         if(chatItem.getSender().equals(username))
         {
-            RelativeLayout.LayoutParams paramsMsg = (RelativeLayout.LayoutParams)userMessage.getLayoutParams();
+
             paramsMsg.addRule(RelativeLayout.ALIGN_PARENT_RIGHT, RelativeLayout.TRUE);
             paramsMsg.addRule(RelativeLayout.ALIGN_PARENT_LEFT, 0);
 
-            RelativeLayout.LayoutParams paramsDate = (RelativeLayout.LayoutParams)userDate.getLayoutParams();
-            paramsDate.addRule(RelativeLayout.ALIGN_PARENT_RIGHT, RelativeLayout.TRUE);
-            paramsDate.addRule(RelativeLayout.ALIGN_PARENT_LEFT, 0);
+//            RelativeLayout.LayoutParams paramsDate = (RelativeLayout.LayoutParams)userDate.getLayoutParams();
+//            paramsDate.addRule(RelativeLayout.ALIGN_PARENT_RIGHT, RelativeLayout.TRUE);
+//            paramsDate.addRule(RelativeLayout.ALIGN_PARENT_LEFT, 0);
 
-            userMessage.setLayoutParams(paramsMsg);
+            stickerImg.setLayoutParams(paramsMsg);
 //            userMessage.setBackgroundResource(R.drawable.rounded_corner_sent);
 
-            userDate.setLayoutParams(paramsDate);
+//            userDate.setLayoutParams(paramsDate);
         }
         else{
-            RelativeLayout.LayoutParams paramsMsg = (RelativeLayout.LayoutParams)userMessage.getLayoutParams();
             paramsMsg.addRule(RelativeLayout.ALIGN_PARENT_LEFT, RelativeLayout.TRUE);
             paramsMsg.addRule(RelativeLayout.ALIGN_PARENT_RIGHT, 0);
 
-            RelativeLayout.LayoutParams paramsDate = (RelativeLayout.LayoutParams)userDate.getLayoutParams();
-            paramsDate.addRule(RelativeLayout.ALIGN_PARENT_LEFT, RelativeLayout.TRUE);
-            paramsDate.addRule(RelativeLayout.ALIGN_PARENT_RIGHT, 0);
+//            RelativeLayout.LayoutParams paramsDate = (RelativeLayout.LayoutParams)userDate.getLayoutParams();
+//            paramsDate.addRule(RelativeLayout.ALIGN_PARENT_LEFT, RelativeLayout.TRUE);
+//            paramsDate.addRule(RelativeLayout.ALIGN_PARENT_RIGHT, 0);
 
-            userMessage.setLayoutParams(paramsMsg);
+            stickerImg.setLayoutParams(paramsMsg);
 //            userMessage.setBackgroundResource(R.drawable.rounded_corner_received);
-            userDate.setLayoutParams(paramsDate);
+//            userDate.setLayoutParams(paramsDate);
         }
 
 //        if (chatItem.getSender().equals(username)) {
@@ -90,20 +92,23 @@ public class ChatActivityViewHolder extends RecyclerView.ViewHolder {
 
         // TODO: Get the sticker id from chatItem and set the image from resource.
 
-        userDate.setText(chatItem.getUserDate());
-        //userMessage.setText(chatItem.getSender());
+        //userDate.setText(chatItem.getUserDate());
+        //userMessage.setText("");
 
         // TODO:
         Integer stickerID = chatItem.getStickerId();
         if (stickerID == 1) {
-            //userMessage.setImageResource(R.mipmap.sticker1_round);
-            userMessage.setText("This is the first image");
+            this.stickerImg.setImageResource(R.mipmap.sticker1_round);
+            //userMessage.setText("This is the first image");
         } else if (stickerID == 2) {
-            userMessage.setText("This is the second image");
+            this.stickerImg.setImageResource(R.mipmap.sticker2_round);
+            //userMessage.setText("This is the second image");
         } else if (stickerID == 3) {
-            userMessage.setText("This is the third image");
+            this.stickerImg.setImageResource(R.mipmap.sticker3_round);
+            //userMessage.setText("This is the third image");
         } else {
-            userMessage.setText("This is the fourth image");
+            this.stickerImg.setImageResource(R.mipmap.sticker4_round);
+            //userMessage.setText("This is the fourth image");
         }
     }
 }
